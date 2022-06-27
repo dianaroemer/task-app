30df9352e76a4c14011fc5bbfd75b36920206a4a
dominicroemer <dominicroemer@gmail.com>
2022-06-27 16:22:44 -0700

    Successful implementation of desired functionality before code cleanup and unique key implementation

--- src/App.js
+++ src/App.js
@@ -1,35 +1,90 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react'
 import Overview from './Components/Overview'
 
-function App() {
-  return (
+class App extends Component {
+  constructor(props) {
+      super(props);
 
-      <div className="App">
+      this.onClickBtn = this.onClickBtn.bind(this);
+      this.pushToInputArr = this.pushToInputArr.bind(this);
+      // this.updateInput = this.updateInput.bind(this);
 
-        <h2>I am App.js, whats happening</h2>
-        <Overview />
+      this.state = {
+        input: '',
+        inputArr: ['test 1', 'test 2', 'test3'],
+      };
+  }
 
-      </div>
+  pushToInputArr(content) {
+    let newArr = this.state.inputArr;
+    newArr.push(content);
+    this.setState({
+      inputArr: newArr,
+    });
+    console.log(this.state.inputArr)
+  }
+
+  // updateInput(incoming) {
+  //   this.setState({
+  //     input: incoming,
+  //   })
+  //   console.log('Youve called updateInput!');
+  // }
+
+  onClickBtn(e) {
+    e.preventDefault();
+    console.log('Button has been clicked!');
+    // console.log(e);
+    console.log(this.state.input);
+    console.log(e.target.form[0].value);
+    if(e.target.form[0].value !== '') {
+      this.pushToInputArr(e.target.form[0].value);
+    }
+  }
+
+  // inputArrToRows() {
+  //   this.state.inputArr.forEach((inputElement) => {
+      
+  //     this.state.rows.push(
+  //         <ListElement 
+  //           element={inputElement} />
+  //     );
+  //   });
+  // }
+
+  render () {
+    return (
+        <div className="App">
+
+          <h2>I am App.js, whats happening!</h2>
+          <Overview title="Content Container" 
+            onButtonClick={this.onClickBtn} 
+            inputArr={this.state.inputArr}/>
+            {/* // updateInput={this.updateInput}/> */}
+
+        </div>
 
 
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
-  );
+      // <div className="App">
+      //   <header className="App-header">
+      //     <img src={logo} className="App-logo" alt="logo" />
+      //     <p>
+      //       Edit <code>src/App.js</code> and save to reload.
+      //     </p>
+      //     <a
+      //       className="App-link"
+      //       href="https://reactjs.org"
+      //       target="_blank"
+      //       rel="noopener noreferrer"
+      //     >
+      //       Learn React
+      //     </a>
+      //   </header>
+      // </div>
+    );
+  }
 }
 
 export default App;
--- src/Components/Overview.js
+++ src/Components/Overview.js
@@ -1,22 +1,49 @@
 import React, { Component } from 'react'
+import reportWebVitals from '../reportWebVitals';
 
 class Overview extends Component {
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
     }
 
+
+
     render () {
+        let rows = [];
+
+        this.props.inputArr.map((inputElement) => {
+            rows.push(
+                <ListElement 
+                  element={inputElement} key={inputElement} />
+            );
+        });
+
         return (
             <div className="Overview">
-                <h3>I am a component from Overview.js</h3>
-                <input className='inputField'></input>
-                <div></div>
-                <button className="inputButton">Submit</button>
-                <div className="inputContent">Content</div>
-
+                <h3>I am a component from Overview.js!</h3>
+                <form>
+                    <input className='inputField'></input>
+                    <p></p>
+                    <button onClick={this.props.onButtonClick}>Submit</button>
+                </form>
+                <h4>{this.props.title}</h4>
+                <div className="contentContainer">
+                    {rows}
+                </div>
             </div>
         )
     }
 }
+
+class ListElement extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render () {
+        return <li className="inputListElement">{this.props.element}</li>
+    }
+}
+
 
 export default Overview;
